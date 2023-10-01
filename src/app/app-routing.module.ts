9f5a47031f48e58e2dffe5f82fe862ec9aa9f9ae
Atarian-ByVoid <cgomespimentel666@gmail.com>
2023-10-01 12:31:32 -0300

    none

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { CadastarCriancaComponent } from './cadastar-crianca/cadastar-crianca.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { HomeComponent } from './home/home.component';
@@ -7,15 +8,15 @@ import { LoginComponent } from './login/login.component';
 import { StyleComponentComponent } from './material/style-component/style-component.component';
 import { TabelaComponent } from './tabela/tabela.component';
 const routes: Routes = [
-
+  { path: 'login', component: LoginComponent },
   { path: 'cadastro', component: CadastroComponent },
   { path: 'style', component: StyleComponentComponent },
-  { path: 'tabela', component: TabelaComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'tabela', component: TabelaComponent, },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'cadastar-crianca', component: CadastarCriancaComponent },
-  { path: '', component: LoginComponent },
-
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes),],
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,17 +8,25 @@ import { Injectable } from '@angular/core';
 export class AuthService {
   private baseUrl = 'http://localhost:3000';
   isAuthenticated: boolean = false;
+
   constructor(private http: HttpClient) { }
 
-  login(email: string, senha: string) {
+  login(email: string, senha: string): Observable<any> {
     const loginData = {
       email: email,
       senha: senha,
     };
 
-    const headers = new HttpHeaders({
-    });
+    const headers = new HttpHeaders({});
 
     return this.http.post(`${this.baseUrl}/auth/login`, loginData, { headers: headers });
+  }
+
+  setAuthenticated(value: boolean) {
+    this.isAuthenticated = value;
+  }
+
+  logout() {
+    this.isAuthenticated = false;
   }
 }
