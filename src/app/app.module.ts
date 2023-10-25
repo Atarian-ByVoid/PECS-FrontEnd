015bf6c980febaf96b8e3a65654120304d366bd3
Atarian-ByVoid <cgomespimentel666@gmail.com>
2023-10-25 00:39:32 -0300

    refacotr: configuração de componentes  globais e adições de imagens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { KeysPipe } from 'src/utils/keys-pipe';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,11 +24,14 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { ErrorPopupComponent } from './error-popup/error-popup.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './material/footer/footer.component';
 import { ImageSelectorComponent } from './material/image-selector/image-selector.component';
 import { NavBarComponent } from './material/nav-bar/nav-bar.component';
 import { StyleComponentComponent } from './material/style-component/style-component.component';
+import { TickerComponent } from './material/ticker/ticker.component';
 import { MeuPerfilComponent } from './meu-perfil/meu-perfil.component';
 import { TabelaComponent } from './tabela/tabela.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -42,6 +46,8 @@ import { TabelaComponent } from './tabela/tabela.component';
     ImageSelectorComponent,
     MeuPerfilComponent,
     KeysPipe,
+    FooterComponent,
+    TickerComponent,
   ],
   imports: [
     BrowserModule,
@@ -61,6 +67,7 @@ import { TabelaComponent } from './tabela/tabela.component';
     MatDialogModule,
     MatTabsModule,
     MatIconModule,
+    FontAwesomeModule,
     RouterModule.forRoot([]),
   ],
   providers: [
